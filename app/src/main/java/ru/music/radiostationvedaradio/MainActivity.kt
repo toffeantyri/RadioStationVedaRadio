924481de926b1ff9462e4aeb50163a1a707938c1
toffeantyri <50056081+toffeantyri@users.noreply.github.com>
2022-04-10 16:24:41 +0500

    Initial commit

---
+++ app/src/main/java/ru/music/radiostationvedaradio/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.music.radiostationvedaradio
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
