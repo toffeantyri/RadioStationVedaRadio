073b1bd287db85f4bfdd8532ed02343948754ab0
tofy3434@yandex.ru <toffeantyri@gmail.com>
2022-04-10 19:30:04 +0500

    0.02 Добавлен ActionBar с кнопками play и Pause Изменена тема ? может надло материал

--- app/src/main/java/ru/music/radiostationvedaradio/MainActivity.kt
+++ app/src/main/java/ru/music/radiostationvedaradio/MainActivity.kt
@@ -1,19 +1,24 @@
 package ru.music.radiostationvedaradio
 
+import android.app.ActionBar
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
 
     lateinit var myMediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setUpActionBar()
         myMediaPlayer = MediaPlayer().apply {
             setAudioAttributes(
                 AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
@@ -24,15 +29,21 @@ class MainActivity : FragmentActivity() {
         }
 
 
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-        btn_start.setOnClickListener {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_play) {
             myMediaPlayer.start()
         }
-
-        btn_stop.setOnClickListener {
+        if (item.itemId == R.id.action_pause) {
             myMediaPlayer.pause()
         }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onStop() {
@@ -42,6 +53,16 @@ class MainActivity : FragmentActivity() {
 
     override fun onResume() {
         super.onResume()
+    }
+
+
+    private fun setUpActionBar() {
+        val ab = supportActionBar
+        ab?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_baseline_radio_24)
+        }
+
 
     }
 }
---
+++ app/src/main/java/ru/music/radiostationvedaradio/RadioMusicService.kt
@@ -0,0 +1,12 @@
+package ru.music.radiostationvedaradio
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class RadioMusicService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
