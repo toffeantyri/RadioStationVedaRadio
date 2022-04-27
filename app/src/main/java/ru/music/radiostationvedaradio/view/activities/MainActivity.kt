552bf24492440a63a1d36b76b73843c49cb500f1
toffeantyri <tofy3434@yandex.ru>
2022-04-27 10:31:06 +0600

    270422 0.466 Методы вынесены из MainActivityu В BaseActivity

--- app/src/main/java/ru/music/radiostationvedaradio/view/activities/MainActivity.kt
+++ app/src/main/java/ru/music/radiostationvedaradio/view/activities/MainActivity.kt
@@ -55,55 +55,9 @@ class MainActivity : BaseMainActivity() {
         super.onDestroy()
     }
 
-    private fun playAudio(urlStream: String) {
-        if (!serviceBound) {
-            Log.d("MyLog", "StartService")
-            val playerIntent = Intent(this@MainActivity, RadioPlayerService::class.java)
-            if (this.isServiceRunning(RadioPlayerService::class.java)) {
-                playerIntent.putExtra(TAG_FIRST_RUN, false)
-            } else {
-                playerIntent.putExtra(TAG_FIRST_RUN, true)
-                playerIntent.putExtra(TAG_NEW_AUDIO_URL, urlStream)
-            }
-            this@MainActivity.startForegroundService(playerIntent)
-            bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE)
-        } else {
-            Log.d("MyLog", "service is already bound")
-            val broadcastIntent = Intent(Broadcast_NEW_AUDIO)
-            broadcastIntent.putExtra(TAG_NEW_AUDIO_URL, urlStream)
-            broadcastIntent.putExtra(TAG_FIRST_RUN, false)
-            sendBroadcast(broadcastIntent)
-        }
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.action_play && mediaService != null) {
-            Log.d("MyLog", "action_play click. isPlaying: ${dataModel.statusMediaPlayer.value}")
-            when (dataModel.statusMediaPlayer.value) {
-                InitStatusMediaPlayer.INIT_COMPLETE -> mediaService?.playMedia()
-                InitStatusMediaPlayer.PLAYING -> mediaService?.pauseMedia()
-                InitStatusMediaPlayer.IDLE -> playAudio(url)
-                InitStatusMediaPlayer.INITIALISATION -> {
-                    Toast.makeText(this, "Loading", Toast.LENGTH_SHORT).show()
-                }
-            }
-        } else if (item.itemId == R.id.action_refresh) {
-            playAudio(url)
-        } else if (item.itemId == R.id.action_low_quality) {
-            url = getString(R.string.veda_radio_stream_link_low)
-            playAudio(url)
-        } else if (item.itemId == R.id.action_medium_quality) {
-            url = getString(R.string.veda_radio_stream_link_medium)
-            playAudio(url)
-        } else if (item.itemId == R.id.action_high_quality) {
-            url = getString(R.string.veda_radio_stream_link_high)
-            playAudio(url)
-        } else if (item.itemId == android.R.id.home) {
-            if (drawer_menu.isDrawerOpen(GravityCompat.START)) drawer_menu.closeDrawer(GravityCompat.START)
-            else drawer_menu.openDrawer(GravityCompat.START)
-        }
-        return super.onOptionsItemSelected(item)
-    }
+
+
 
 
 }
