a25186fa4b3b2366f9861c156f03c9851d7c4a38
toffeantyri <tofy3434@yandex.ru>
2022-05-29 19:55:13 +0500

    Toolbar fix.
    - add style for Toolbar
    - add gravity inside toolbar
    - add titleTextColor White

--- app/src/main/java/ru/music/radiostationvedaradio/utils/Constants.kt
+++ app/src/main/java/ru/music/radiostationvedaradio/utils/Constants.kt
@@ -1,6 +1,8 @@
 package ru.music.radiostationvedaradio.utils
 
 
+import android.annotation.SuppressLint
 import ru.music.radiostationvedaradio.activityes.MainActivity
 
+@SuppressLint("StaticFieldLeak")
 internal var APP_CONTEXT : MainActivity? = null
