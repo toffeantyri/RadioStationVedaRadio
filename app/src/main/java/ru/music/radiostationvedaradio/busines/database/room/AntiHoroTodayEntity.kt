1272f96fda48fa31baabdcca743bf9ee5f0963fe
toffeantyri <tofy3434@yandex.ru>
2022-06-03 10:55:28 +0600

    Add Converter for List String ROOM
    - add annotation typeconverts for database class
    - add class HoroListConverter with 2 funcs listtostring and stringtolist

--- app/src/main/java/ru/music/radiostationvedaradio/busines/database/room/AntiHoroTodayEntity.kt
+++ app/src/main/java/ru/music/radiostationvedaradio/busines/database/room/AntiHoroTodayEntity.kt
@@ -1,13 +1,17 @@
 package ru.music.radiostationvedaradio.busines.database.room
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
 
 @Entity(tableName = "anti_goro_id_date")
 data class AntiHoroTodayEntity (
 
     @PrimaryKey val date : String = "",
-    //@ColumnInfo val list : List<String>
+
+    @ColumnInfo
+    val list : List<String>
 
 
 
