87d5b2adbae948e0b24e9f7ac15c911bd18633db
toffeantyri <tofy3434@yandex.ru>
2022-06-04 09:54:04 +0500

    Add func HoroscopeModelClasses to List of List<HoroItemHolder>
    - return 3 list:
    - today list<HoroItem...>
    - tomorrow list<HoroItem...>
    - tomorrow2 list<HoroItem...>

--- app/src/main/java/ru/music/radiostationvedaradio/utils/funcs.kt
+++ app/src/main/java/ru/music/radiostationvedaradio/utils/funcs.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavController
 import ru.music.radiostationvedaradio.R
 import ru.music.radiostationvedaradio.activityes.MainActivity
 import ru.music.radiostationvedaradio.busines.database.room.AntiHoroTodayEntity
+import ru.music.radiostationvedaradio.busines.model.antihoro.HoroItemHolder
 import ru.music.radiostationvedaradio.busines.model.antihoro.HoroscopeModelClasses
 import java.text.SimpleDateFormat
 import java.util.*
@@ -63,7 +64,8 @@ fun HoroscopeModelClasses.getTodayHoroList(): List<String> {
     return list
 }
 
-fun List<String>.listHoroToEntity(): AntiHoroTodayEntity = AntiHoroTodayEntity(date = this[0], list = this)
+fun List<String>.listHoroToEntity(): AntiHoroTodayEntity =
+    AntiHoroTodayEntity(date = this[0], list = this)
 
 fun getTodayDate(format: String): String {
     val date = Calendar.getInstance().time
@@ -71,5 +73,50 @@ fun getTodayDate(format: String): String {
     return formatter.format(date)
 }
 
-
+fun HoroscopeModelClasses.toListHoroItemClass(): List<List<HoroItemHolder>> {
+    val allList = arrayListOf<List<HoroItemHolder>>()
+    val listToday = arrayListOf<HoroItemHolder>()
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Овен", this.aries?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Телец", this.taurus?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Близнецы", this.gemini?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Рак", this.cancer?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Лев", this.leo?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Дева", this.virgo?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Весы", this.libra?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Скорпион", this.scorpio?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Стрелец", this.sagittarius?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Козерог", this.capricorn?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Водолей", this.aquarius?.get(1) ?: ""))
+    listToday.add(HoroItemHolder(this.date?.today ?: "", "Рыбы", this.pisces?.get(1) ?: ""))
+    val tom = arrayListOf<HoroItemHolder>()
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Овен", this.aries?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Телец", this.taurus?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Близнецы", this.gemini?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Рак", this.cancer?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Лев", this.leo?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Дева", this.virgo?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Весы", this.libra?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Скорпион", this.scorpio?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Стрелец", this.sagittarius?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Козерог", this.capricorn?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Водолей", this.aquarius?.get(2) ?: ""))
+    tom.add(HoroItemHolder(this.date?.tomorrow ?: "", "Рыбы", this.pisces?.get(2) ?: ""))
+    val tom2 = arrayListOf<HoroItemHolder>()
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Овен", this.aries?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Телец", this.taurus?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Близнецы", this.gemini?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Рак", this.cancer?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Лев", this.leo?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Дева", this.virgo?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Весы", this.libra?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Скорпион", this.scorpio?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Стрелец", this.sagittarius?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Козерог", this.capricorn?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Водолей", this.aquarius?.get(3) ?: ""))
+    tom2.add(HoroItemHolder(this.date?.tomorrow02 ?: "", "Рыбы", this.pisces?.get(3) ?: ""))
+    allList[0] = listToday
+    allList[1] = tom
+    allList[2] = tom2
+    return allList
+}
 
